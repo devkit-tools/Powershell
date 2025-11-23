b12d79c14df9263d3770960154cd4b4f6459ee62
devkit-tools <devkit.romania@gmail.com>
2025-11-23 19:03:44 +0100

    Update query_documents.ps1

--- query_documents.ps1
+++ query_documents.ps1
@@ -1,7 +1,8 @@
-$exts = ".xls",".xls*",".xltx",".od*",".doc",".doc*",".pdf",".pot",".pot*",".pp*"
+$exts = "*.xls","*.xltx","*.od*","*.doc","*.doc*","*.pdf","*.pot","*.pot*","*.pp*","*.do"
 
 foreach ($ext in $exts) {
     Write-Host "`nFile extension: $ext"
-    Get-ChildItem -Path C:\ -Recurse -Filter $ext -ErrorAction SilentlyContinue |
-        Where-Object {$_ -notmatch "lib|fonts|share|core"}
+    Get-ChildItem -Path C:\ -Recurse -Include $ext -File -ErrorAction SilentlyContinue |
+        Where-Object { $_.FullName -notmatch "lib|fonts|share|core" } |
+        Select-Object FullName
 }
